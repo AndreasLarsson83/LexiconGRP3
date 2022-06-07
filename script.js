93d36653b123babf812380470560b249c5b29a93
Andreas Larsson <andreas.l83@live.com>
2022-06-07 12:02:07 +0200

    Some Js changes

--- script.js
+++ script.js
@@ -41,3 +41,15 @@ function openTab(evt, tabName) {
 }
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar-top").style.top = "0";
+  } else {
+    document.getElementById("navbar-top").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
